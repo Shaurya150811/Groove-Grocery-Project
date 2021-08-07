6630e172205a8f7ff37ad58791a241fee999e69a
Shaurya150811 <87265614+Shaurya150811@users.noreply.github.com>
2021-08-07 18:09:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+<script>
+    function buy() {
+        document.getElementById("buy-now").style.display="none";
+        document.getElementById("thanks").style.display="inherit";
+
+    }
+</script>
